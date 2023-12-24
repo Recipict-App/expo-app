3ddbc4f6ac80a8382c910a50e9d6774d6775f39f
draihan <draihan@student.ubc.ca>
2023-12-24 00:42:21 -0800

    Add expo router

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! erik gei</Text>
+      <Text>ROUTTERERERERE</Text>
       <StatusBar style="auto" />
     </View>
   );

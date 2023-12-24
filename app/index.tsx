d89cece9723945b82e490183f783b8556c187a8b
Erick Jovan Muljadi <ejovan2004@gmail.com>
2023-12-24 04:26:47 -0800

    Fix index

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { Redirect } from "expo-router";
 
 export default function App() {
   return <Redirect href="/home" />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

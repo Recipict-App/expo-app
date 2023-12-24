156ae136712a2c090e506df17c4b46be072bd3ff
draihan <draihan@student.ubc.ca>
2023-12-24 01:38:49 -0800

    Splash screen

--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ROUTTEREREREREwkwkkwkkw</Text>
+      <Text>Onboarding</Text>
       <StatusBar style="auto" />
     </View>
   );

00a9b34e0a5e8286f615a563c770329a470fbc22
draihan <draihan@student.ubc.ca>
2024-02-22 01:55:03 -0800

    Delete unnecessary files and code

--- backend/helpers/userHelper.ts
+++
@@ -1,8 +0,0 @@
-import User from "../models/user";
-
-export const sendCreatedUser = async (user: any) => {
-  const newUser = new User(user);
-  const data = await newUser.save();
-
-  return data;
-};

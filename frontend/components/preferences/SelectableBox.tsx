d0d3877aa7adf66b4a3c60a03db81d370c94ed45
draihan <draihan@student.ubc.ca>
2024-03-06 01:31:26 -0800

    Fix search functionality and add cuisine selection in PreferenceSheet

---
+++ frontend/components/preferences/SelectableBox.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+export default function SelectableBox({ label, isSelected, onToggle }: any) {
+  return (
+    <TouchableOpacity onPress={onToggle}>
+      <View
+        className={`px-4 py-3 rounded-3xl items-center justify-center ${
+          isSelected ? "bg-green" : "bg-white border border-green" 
+        }`}
+
+      >
+        <Text
+          className={`font-pps text-base ${
+            isSelected ?  "text-white" : "text-green"
+          }`}
+        >
+          {label}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
--- frontend/components/sheets/PreferenceSheet.tsx
+++ frontend/components/sheets/PreferenceSheet.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ActionSheet, {
   SheetProps,
   SheetManager,
@@ -5,11 +6,63 @@ import ActionSheet, {
 
 import { View, Text, TouchableOpacity } from "react-native";
 
+import SelectableBox from "../preferences/SelectableBox";
+
+export enum cuisinesEnum {
+  African = "African",
+  Asian = "Asian",
+  American = "American",
+  British = "British",
+  Cajun = "Cajun",
+  Caribbean = "Caribbean",
+  Chinese = "Chinese",
+  Eastern_European = "Eastern European",
+  European = "European",
+  French = "French",
+  German = "German",
+  Greek = "Greek",
+  Indian = "Indian",
+  Irish = "Irish",
+  Italian = "Italian",
+  Japanese = "Japanese",
+  Jewish = "Jewish",
+  Korean = "Korean",
+  Latin_American = "Latin American",
+  Mediterranean = "Mediterranean",
+  Mexican = "Mexican",
+  Middle_Eastern = "Middle Eastern",
+  Nordic = "Nordic",
+  Southern = "Southern",
+  Spanish = "Spanish",
+  Thai = "Thai",
+  Vietnamese = "Vietnamese",
+}
+
 const handleClose = () => {
-  SheetManager.hide("edit-ingredients-sheet");
+  SheetManager.hide("preference-sheet");
+};
+
+const getSelectedLabels = (selectedBoxes: Record<string, boolean>): string => {
+  return Object.keys(selectedBoxes)
+    .filter((key) => selectedBoxes[key])
+    .join(", ");
 };
 
 export default function PreferenceSheet(props: SheetProps) {
+  const [selectedBoxes, setSelectedBoxes] = useState<Record<string, boolean>>(
+    {}
+  );
+
+  const toggleSelect = (label: string) => {
+    setSelectedBoxes((prevState) => ({
+      ...prevState,
+      [label]: !prevState[label],
+    }));
+  };
+
+  const selectedLabels = getSelectedLabels(selectedBoxes);
+  console.log(selectedLabels);
+
   return (
     <ActionSheet id={props.sheetId}>
       <View className="w-full h-fit min-h-[70%] py-2 items-center rounded-xl">
@@ -24,12 +77,19 @@ export default function PreferenceSheet(props: SheetProps) {
         >
           <TouchableOpacity onPress={handleClose} />
         </View>
-
         <Text className="font-pps text-2xl text-center mt-6">
           Choose your diets
         </Text>
-
-
+        <View className="bg-grey grid-flow-row-dense w-full">
+          {Object.values(cuisinesEnum).map((cuisine) => (
+            <SelectableBox
+              key={cuisine}
+              label={cuisine}
+              isSelected={selectedBoxes[cuisine]}
+              onToggle={() => toggleSelect(cuisine)}
+            />
+          ))}
+        </View>
       </View>
     </ActionSheet>
   );

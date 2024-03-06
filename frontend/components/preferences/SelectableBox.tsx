069ff7bb44fa6007e75be443fb641ceb861b5a84
Erick Jovan Muljadi <ejovan2004@gmail.com>
2024-03-06 01:50:01 -0800

    fix ui bug

--- frontend/components/preferences/SelectableBox.tsx
+++ frontend/components/preferences/SelectableBox.tsx
@@ -3,16 +3,16 @@ import { View, Text, TouchableOpacity } from "react-native";
 
 export default function SelectableBox({ label, isSelected, onToggle }: any) {
   return (
-    <TouchableOpacity onPress={onToggle}>
+    <TouchableOpacity onPress={onToggle} className="" style={{alignSelf: "flex-start"}}>
       <View
         className={`px-4 py-3 rounded-3xl items-center justify-center ${
-          isSelected ? "bg-green" : "bg-white border border-green" 
+          isSelected ? "bg-green" : "bg-white border border-green"
         }`}
 
       >
         <Text
           className={`font-pps text-base ${
-            isSelected ?  "text-white" : "text-green"
+            isSelected ? "text-white" : "text-green"
           }`}
         >
           {label}

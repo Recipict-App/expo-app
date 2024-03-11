c941ff680a195032335d97ae058405f744399bdf
draihan <draihan@student.ubc.ca>
2024-03-11 03:10:52 -0700

    Add @fawazahmed/react-native-read-more package

--- frontend/components/sheets/RecipeIngredientSheet.tsx
+++ frontend/components/sheets/RecipeIngredientSheet.tsx
@@ -1,10 +1,12 @@
 import ActionSheet, { SheetProps } from "react-native-actions-sheet";
-import { View, Text, FlatList, ScrollView } from "react-native";
+import { View, Text, FlatList, ScrollView, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { SheetManager } from "react-native-actions-sheet";
 import { Image } from "expo-image";
 import { useWindowDimensions } from "react-native";
 import { recipeType } from "../../types/recipe-type";
+
+import ReadMore from "@fawazahmed/react-native-read-more";
 
 const handleCloseRecipe = () => {
   SheetManager.hide("recipe-ingredient-sheet");
@@ -21,24 +23,22 @@ export default function RecipeIngredientSheet(
     return (
       <View className="h-full flex items-center px-6" style={{ width: width }}>
         {/* Title */}
-        <View className="flex max-h-[10vh] w-full mt-4 px-4 ">
+        <View className="w-full mt-4">
           <Text
-            className="w-full font-pps text-2xl text-center"
+            className="w-full font-pps text-xl text-center"
             adjustsFontSizeToFit
           >
             {props.payload?.recipe.title}
           </Text>
         </View>
-
         {/* Image */}
-        <View className="w-full h-1/3 rounded-2xl overflow-hidden justify-center items-center mb-6 mt-4">
+        <View className="w-full h-2/5 rounded-2xl overflow-hidden justify-center items-center mb-6 mt-4">
           <Image
             className="w-[130%] h-[130%]"
             source={{ uri: props.payload?.recipe.image }}
             contentFit="contain"
           />
         </View>
-
         {/* STATS */}
         <View className="w-full flex flex-row justify-between px-2">
           <View className="h-[50px] w-fit bg-[#C9C8F6] rounded-2xl flex-row items-center px-2">
@@ -47,7 +47,7 @@ export default function RecipeIngredientSheet(
               source={require("../../assets/icons/Clock.svg")}
             />
 
-            <Text className="ml-1 font-pps text-base text-[#5A58B5]">
+            <Text className="ml-1 font-medium text-base text-[#5A58B5]">
               {props.payload?.recipe.readyInMinutes || "-"} Mins
             </Text>
           </View>
@@ -57,7 +57,7 @@ export default function RecipeIngredientSheet(
               style={{ width: 32, height: 32 }}
               source={require("../../assets/icons/People.svg")}
             />
-            <Text className="ml-1 font-pps text-base text-[#55674D]">
+            <Text className="ml-1 font-medium text-base text-[#55674D]">
               {props.payload?.recipe.servings || "-"}
             </Text>
           </View>
@@ -67,22 +67,25 @@ export default function RecipeIngredientSheet(
               style={{ width: 32, height: 32 }}
               source={require("../../assets/icons/Fire.svg")}
             />
-            <Text className="ml-1 font-pps text-base text-[#949143]">
+            <Text className="ml-1 font-medium text-base text-[#949143]">
               {props.payload?.recipe.calories || "-"} Cals
             </Text>
           </View>
         </View>
-
+        {/* line */}
+        <View className="w-[98%] border-b-[1px] my-4 border-b-grey" />
         {/* Summary */}
-
-        <View className="w-full mt-6 px-1">
+        <View className="w-full px-2">
           <ScrollView
-            style={{ height: 180 }}
+            style={{ height: 200 }}
             showsVerticalScrollIndicator={false}
           >
-            <Text className="font-pp text-justify text-lg leading-6">
-              {props.payload?.recipe.Cleanedsummary}
+            <Text className="font-semibold text-lg mb-1 tracking-wider">
+              What's this?
             </Text>
+            <ReadMore numberOfLines={5} style={SummaryStyle.textStyle}>
+              {props.payload?.recipe.Cleanedsummary}
+            </ReadMore>
           </ScrollView>
         </View>
       </View>
@@ -93,9 +96,7 @@ export default function RecipeIngredientSheet(
     return (
       <View className="h-full flex items-center px-6" style={{ width: width }}>
         {/* Ingredient  */}
-        <Text className="font-pps text-xl text-center mt-6 px-4 mb-3">
-          Ingredients
-        </Text>
+        <Text className="font-pps text-xl  mt-6 px-4 mb-3">Ingredients</Text>
 
         {/* items */}
         <View className="w-full h-5/6 flex flex-row justify-between px-2">
@@ -103,9 +104,13 @@ export default function RecipeIngredientSheet(
             data={props.payload?.recipe.totalIngredients}
             renderItem={({ item, index }) => {
               return (
-                <View className="bg-[#F8F8F6] w-full rounded-md  flex-row items-center mt-2">
-                  <View className="rounded-l-md h-full w-[3px] bg-grey"></View>
-                  <Text className="ml-1 text-sm text-[#5A58B5] px-1 py-1">
+                <View className=" w-full rounded-md flex flex-row items-center ">
+                  <Image
+                    style={{ width: 34, height: 34 }}
+                    source={require("../../assets/icons/CheckIcon.svg")}
+                    className="mt-2"
+                  />
+                  <Text className="text-sm text-[#5A58B5] px-1 ">
                     ({item.amount.toString().substring(0, 5)} {item.unit}){" "}
                     <Text className="font-pps">{item.name}</Text>
                   </Text>
@@ -134,14 +139,16 @@ export default function RecipeIngredientSheet(
             data={props.payload?.recipe.instructions}
             renderItem={({ item, index }) => {
               return (
-                <View className="bg-[#F8F8F6] w-full rounded-md flex-row items-center mt-2">
-                  <View className="rounded-l-md h-full w-1/12 py-1 bg-green items-center">
-                    <Text className="text-white text-base font-ppb">
-                      {index + 1}
-                    </Text>
+                <View className="w-full rounded-md flex-row items-center mt-3">
+                  <View className="h-full items-center">
+                    <View className="bg-green px-1 py-1 aspect-square rounded-full">
+                      <Text className=" text-center font-pps text-white ">
+                        {index + 1}
+                      </Text>
+                    </View>
                   </View>
 
-                  <Text className="w-5/6 ml-3 text-sm font-medium text-green py-2 text-justify">
+                  <Text className="w-5/6 ml-3 text-sm leading-5 text-justify">
                     {item.step}
                   </Text>
                 </View>
@@ -160,11 +167,11 @@ export default function RecipeIngredientSheet(
 
   return (
     <ActionSheet id={props.sheetId}>
-      <View className="w-full h-fit min-h-[70%] py-2 pb-9 items-center rounded-xl">
+      <View className="w-full h-fit min-h-[70%] max-h-[95%] py-2 pb-9 items-center rounded-xl  justify-between">
         {/* top bar */}
         <View
           style={{
-            width: "50%",
+            width: "20%",
             height: 4,
             borderRadius: 10,
             backgroundColor: "#9F9F9F",
@@ -173,22 +180,24 @@ export default function RecipeIngredientSheet(
           <TouchableOpacity onPress={handleCloseRecipe} />
         </View>
 
-        <FlatList
-          horizontal
-          data={slides}
-          renderItem={({ item }) => {
-            if (item === "SummarySlide") return <SummarySlide />;
-            if (item === "IngredientSlide") return <IngredientSlide />;
-            if (item === "InstructionSlide") return <InstructionSlide />;
-            return <></>;
-          }}
-          keyExtractor={(item, index) => index.toString()}
-          showsHorizontalScrollIndicator={false}
-          pagingEnabled
-        />
+        <View className="w-full">
+          <FlatList
+            horizontal
+            data={slides}
+            renderItem={({ item }) => {
+              if (item === "SummarySlide") return <SummarySlide />;
+              if (item === "IngredientSlide") return <IngredientSlide />;
+              if (item === "InstructionSlide") return <InstructionSlide />;
+              return <></>;
+            }}
+            keyExtractor={(item, index) => index.toString()}
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+          />
+        </View>
 
         {/* footer */}
-        <View className="flex w-full flex-row justify-center items-center space-x-8">
+        <View className="flex w-full flex-row justify-center items-center space-x-8 mt-2">
           {/* Close button */}
           <TouchableOpacity
             className="h-fit w-ful justify-center flex items-center"
@@ -226,3 +235,19 @@ export default function RecipeIngredientSheet(
     </ActionSheet>
   );
 }
+
+const SummaryStyle = StyleSheet.create({
+  safe: {
+    flex: 1,
+  },
+  root: {
+    flex: 1,
+    padding: 16,
+  },
+  textStyle: {
+    fontSize: 16,
+    lineHeight: 25,
+    color: "grey",
+    paddingHorizontal: 2,
+  },
+});

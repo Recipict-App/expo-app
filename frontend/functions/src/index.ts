a39ebdc396a475a20e09e2e7f349a7dfcfa7c19f
draihan <draihan@student.ubc.ca>
2024-05-26 23:43:15 -0700

    chore: Update API endpoint for ingredient properties

--- frontend/functions/src/index.ts
+++ frontend/functions/src/index.ts
@@ -2,26 +2,22 @@
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import { UserRecord } from "firebase-admin/auth";
-import { v4 as uuidv4 } from "uuid";
 
-const { DocumentProcessorServiceClient } = require("@google-cloud/documentai").v1;
 const admin = require("firebase-admin");
-const functions = require("firebase-functions/v1/auth");
-const { onSchedule } = require("firebase-functions/v2/scheduler");
+const auth_functions = require("firebase-functions/v1/auth");
+const pubsub_functions = require("firebase-functions/v1/pubsub");
 
 // * API KEYS
 
-const projectId = "recipict-dev-gcp";
 const SPOONACULAR_API_KEY = "fe291eb222404e7ab4b7858023ff1d90";
-const DocAI_locationId = "us";
-const DocAI_processorId = "93c825aa3842bef3";
+
 
 // * Initialization
 
 admin.initializeApp({
   projectId: "recipict-dev-gcp",
 });
-const DocAI_client = new DocumentProcessorServiceClient();
+
 
 // * ONREQUEST FUNCTIONS
 
@@ -58,109 +54,10 @@ export const recipe_by_random = onRequest(async (req, res) => {
   res.status(200).json(result);
 });
 
-export const receipt_extractor_api = onRequest(async (req, res) => {
-  const base64ImageData = req?.body?.base64ImageData;
-
-  const document_ai_request_body = {
-    name: `projects/${projectId}/locations/${DocAI_locationId}/processors/${DocAI_processorId}`,
-    rawDocument: {
-      content: base64ImageData,
-      mimeType: "image/jpeg",
-    },
-    // fieldMask: "date"
-  };
-
-  let document_ai_result;
-
-  try {
-    // suprisingly, the line below return a list, and we need the first element
-    document_ai_result = await DocAI_client.processDocument(document_ai_request_body);
-    // res.status(200).json(document_ai_result);
-  } catch (error: any) {
-    res.status(500).send("Error when calling DocAI_client");
-  }
-
-  // logger.log("succcessfully called DocAI_client -----------");
-
-  /* ------- GET EXTRACTED DATA AND OBJECTIFY IT -------- */
-
-  const extractProperties = (properties: any[]) => {
-    let product_code = "";
-    let product_name = "";
-    let product_quantity = "1";
-
-    properties.forEach((property: any) => {
-      if (property.type == "product_code") product_code = property.mentionText;
-      if (property.type == "product_name") product_name = property.mentionText;
-      if (property.type == "product_quantity") product_quantity = property.mentionText;
-    });
-
-    return { product_code, product_name, product_quantity };
-  };
-
-  const extractQuantityAndUnit = (product_quantity: string) => {
-    const containsLetter = /[a-zA-Z]/.test(product_quantity);
-    let quantity_unit = "";
-
-    if (containsLetter) {
-      quantity_unit = product_quantity.match(/[a-zA-Z]+/)?.[0] || "";
-      product_quantity = product_quantity.replace(/[a-zA-Z]/g, "");
-    }
-
-    return { product_quantity, quantity_unit };
-  };
-
-  let receipt_date = "";
-  const items: any[] = await Promise.all(
-    document_ai_result[0].document.entities.map(async (item: any) => {
-      if (item.type === "date") {
-        receipt_date = item.mentionText;
-        return;
-      }
-
-      const { product_code, product_name, product_quantity: raw_product_quantity } = extractProperties(item.properties);
-      const { product_quantity, quantity_unit } = extractQuantityAndUnit(raw_product_quantity);
-
-      // get the ingredient properties
-      let { category = "Not ingredients", expiration = "7", generic_name = "" } = {};
-
-      try {
-        let assign_ingredient_properly_url = `https://us-central1-recipict-dev-gcp.cloudfunctions.net/get_ingredient_property_py?name=${product_name}`;
-        const properties_response = await fetch(assign_ingredient_properly_url, {
-          method: "GET",
-          mode: "cors",
-        });
-
-        ({ category, expiration, generic_name } = await properties_response.json());
-      } catch (error) {
-        console.error("An error occurred:", error);
-      }
-
-      // logger.log("generic_name", generic_name);
-
-      return {
-        name: product_name,
-        quantity: product_quantity,
-        unit: quantity_unit || "pc",
-        daysBeforeExpired: expiration,
-        dateAdded: receipt_date || new Date(),
-        type: category,
-        id: uuidv4(),
-        genericName: generic_name,
-        productCode: product_code,
-      };
-    })
-  );
-
-  const filteredItems: any[] = items.filter((item: any) => item !== undefined);
-
-  // create a new object
-  res.status(200).json({ items: filteredItems });
-});
 
 // * EVENT-BASED FUNCTIONS
 
-exports.create_user_event = functions.user().onCreate((user: UserRecord) => {
+exports.create_user_event = auth_functions.user().onCreate((user: UserRecord) => {
   const new_user_data = {
     uid: user.uid,
     name: user.displayName,
@@ -180,10 +77,10 @@ exports.create_user_event = functions.user().onCreate((user: UserRecord) => {
     .set(JSON.parse(JSON.stringify(new_user_data)));
 });
 
-exports.deduct_expiry_date_everyday = onSchedule("every day 00:00", async () => {
+exports.decrement_expiry_date_everyday = pubsub_functions.schedule('every day 00:00').onRun(async (context: any) => {
   try {
     // Fetch all users
-    const usersSnapshot = await admin.firestore().collection("users").get();
+    const usersSnapshot = await admin.firestore().collection('users').get();
     const userDocs = usersSnapshot.docs;
 
     // Process each user document
@@ -201,7 +98,7 @@ exports.deduct_expiry_date_everyday = onSchedule("every day 00:00", async () => 
         });
 
         // Update user document with the new ingredients array
-        await admin.firestore().collection("users").doc(userDoc.id).update({
+        await admin.firestore().collection('users').doc(userDoc.id).update({
           ingredients: updatedIngredients,
         });
       }
@@ -210,8 +107,8 @@ exports.deduct_expiry_date_everyday = onSchedule("every day 00:00", async () => 
     // Wait for all promises to complete
     await Promise.all(promises);
 
-    logger.log("Successfully decremented daysBeforeExpired for all users.");
+    logger.log('Successfully decremented daysBeforeExpired for all users.');
   } catch (error) {
-    logger.log("Error decrementing daysBeforeExpired:", error);
+    logger.log('Error decrementing daysBeforeExpired:', error);
   }
 });
